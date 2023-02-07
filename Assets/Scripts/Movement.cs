62ce9d0f9b00243d55ab45c8548a7bbb1cb4e45a
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-07 22:22:48 +0300

    add Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] private float _speed;
+    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-1 * _speed * Time.deltaTime,0,0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime,0,0);
+        }
+    }
+}
