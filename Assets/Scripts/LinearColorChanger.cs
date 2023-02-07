6a1d4d3e0aaf21a9fba7475cff22f53488379379
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-07 21:39:10 +0300

    Initial commit

---
+++ Assets/Scripts/LinearColorChanger.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LinearColorChanger : MonoBehaviour
+{
+    [SerializeField] private Color _targetColor;
+    [SerializeField] private float _duration;
+    
+    private Color _startColor;
+    
+    private SpriteRenderer _target;
+    
+    private float _runningTime;
+    
+
+    private void Start()
+    {
+        _target = GetComponent<SpriteRenderer>();
+        _startColor = _target.color;
+    }
+
+    private void Update()
+    {
+        if (_runningTime <= _duration)
+        {
+            _runningTime += Time.deltaTime;
+            //Debug.Log(_runningTime);
+            float normalizeRunningTime = _runningTime / _duration;
+            _target.color = Color.Lerp(_startColor, _targetColor, normalizeRunningTime);
+        }
+    }
+}
