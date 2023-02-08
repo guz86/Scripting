fb1abf284605d3d9144e1f4c2a8876374ef70974
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-08 18:15:39 +0300

    add DisableOnClick

--- Assets/Scripts/DisableOnClick.cs
+++ Assets/Scripts/DisableOnClick.cs
@@ -1,9 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
     public class DisableOnClick : MonoBehaviour
     {
+        
+        [SerializeField] private Color _targetColor;
+        [SerializeField] private float _duration;
+        private SpriteRenderer _target;
+        private Color _startColor;
+        [SerializeField] private float _runningTime;
+        
+        
+        private void Start()
+        {
+            _target = GetComponent<SpriteRenderer>();
+            _startColor = _target.color;
+        }
+        
         private void OnMouseDown()
         {
-            gameObject.SetActive(false);
+            StartCoroutine(Fade());
         }
+
+        private IEnumerator Fade()
+        {
+            while (_runningTime <= _duration)
+            {
+                _runningTime += Time.deltaTime;
+                float normalizeRunningTime = _runningTime / _duration;
+                _target.color = Color.Lerp(_startColor, _targetColor, normalizeRunningTime);
+                yield return null;
+            }
+
+            gameObject.SetActive(false);
+            
+        } 
     }
