fc981fd8191fbd204890f38a9702d83c09d49cf4
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-08 20:18:47 +0300

    add EndPoint

---
+++ Assets/Scripts/EndPoint.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    [SerializeField] private Color _reachedColor;
+    
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.TryGetComponent<Player>(out Player player))
+        {
+            _spriteRenderer.color = _reachedColor;
+        }
+    }
+}
