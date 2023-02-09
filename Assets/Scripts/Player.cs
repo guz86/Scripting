f3340e212f8abb65b3f62e1c2682ebee5345e583
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-09 04:49:57 +0300

    fix Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private UnityEvent _hit;
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.TryGetComponent(out Block block))
+        {
+            _hit?.Invoke();
+        }
+    }
 }
