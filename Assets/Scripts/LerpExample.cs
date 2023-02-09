65af04e82ccb07fb706e5e95a3c6828b050b2e3d
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-09 14:32:03 +0300

    add LerpExample

---
+++ Assets/Scripts/LerpExample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LerpExample : MonoBehaviour
+{
+    [SerializeField] private Transform _a;
+    [SerializeField] private Transform _b;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _pathTime;
+    [SerializeField] private float _pathRunningTime;
+
+    private void Update()
+    {
+        _pathRunningTime += Time.deltaTime;
+        _target.position = Vector3.Lerp(_a.position, _b.position, _pathRunningTime / _pathTime);
+    }
+
+    public void SetNormalizePosition(float position)
+    {
+        // _target.position = Vector3.Lerp(_a.position, _b.position, position);
+    }
+}
