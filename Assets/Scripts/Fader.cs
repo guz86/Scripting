a876af62f8c4e7cba4a294f50c4f66e3a6954725
Pavel Gordienko <guzmo6@rambler.ru>
2023-02-09 14:54:42 +0300

    add Fader

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    [SerializeField] private float _duration;
+
+    private void Start()
+    {
+        var fadeInJob = StartCoroutine(FadeIn(_duration));
+    }
+
+    private IEnumerator FadeIn(float duration)
+    {
+        var color = _image.color;
+
+        for (int i = 0; i < 255; i++)
+        {
+            color.a = 1f - (1f / 255f * i);
+            _image.color = color;
+            yield return null;
+        }
+    }
+}
